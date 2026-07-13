0795f7ba7356070ec7ead5a83cbf0338da7b2646
Nikhil <ghantasainikhil7@gmail.com>
2026-07-13 12:45:48 +0530

    Clean up repository to keep only static web files

--- config.py
+++
@@ -1,5 +0,0 @@
-UPLOAD_FOLDER = "uploads"
-
-MODEL_NAME = "facebook/nllb-200-distilled-600M"
-
-MAX_CHUNK_SIZE = 400
--- model/test.py
+++
@@ -1,8 +0,0 @@
-from translator import translate_text
-
-result = translate_text(
-    "Artificial intelligence is transforming the world",
-    "fra_Latn"
-)
-
-print(result)
